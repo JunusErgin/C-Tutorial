a71b757ece69359c30598544b89d8a32ef686281
Junus Ergin <JunusErgin@users.noreply.github.com>
2022-03-21 14:40:09 +0100

    Verzweigungen

--- tutorial.c
+++ tutorial.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 
 int main() {
-    char* receiver[1];
+    char* receiver[256];
+    char* greeting = "Sehr geehrter";
     int attachments = 2;
+    int gender = 2;
     
+    printf("An wen ist der Brief gerichtet? ");
     scanf("%[^\n]", &receiver);
     
-    // Write C code here
-    printf("Sehr geehrter %s,\n\n", receiver);
+    printf("Ich schreibe an (1) Eine Frau (2) Einen Mann ");
+    scanf("%d", &gender);
+    
+    if(gender == 1) {
+        greeting = "Sehr geehrte";
+    }
+    
+    printf("\n\n%s %s,\n\n", greeting, receiver);
     printf("Hiermit übermittel ich ihnen die notwendigen Papiere.\n\n");
     
     printf("Anhänge: %d", attachments);
