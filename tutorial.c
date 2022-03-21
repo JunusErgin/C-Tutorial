2ba214f90818452c408c9834ac0712cfc435b62b
Junus Ergin <JunusErgin@users.noreply.github.com>
2022-03-21 14:39:07 +0100

    Variablen

---
+++ tutorial.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char* receiver = "Frau Müller";
+    int attachments = 2;
+    
+    // Write C code here
+printf("Sehr geehrter %s,\n\n", receiver);
+    printf("Hiermit übermittel ich ihnen die notwendigen Papiere.\n\n");
+    
+    printf("Anhänge: %d", attachments);
+    
+    return 0;
+}
