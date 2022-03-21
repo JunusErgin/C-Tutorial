6b848835496dc3c83eb6116a29dc7cb6c51c4877
Junus Ergin <JunusErgin@users.noreply.github.com>
2022-03-21 14:39:39 +0100

    Scanf

--- tutorial.c
+++ tutorial.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 int main() {
-    char* receiver = "Frau Müller";
+    char* receiver[1];
     int attachments = 2;
     
+    scanf("%[^\n]", &receiver);
+    
     // Write C code here
-printf("Sehr geehrter %s,\n\n", receiver);
+    printf("Sehr geehrter %s,\n\n", receiver);
     printf("Hiermit übermittel ich ihnen die notwendigen Papiere.\n\n");
     
     printf("Anhänge: %d", attachments);
